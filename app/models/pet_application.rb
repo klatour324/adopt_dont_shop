99c93ba3da80791b60e71a3c82a46f36adfb1a2c
Katy La Tour <klatour324@gmail.com>
2021-02-17 13:33:00 -0600

    Create validations for pet_id and application_id on pet_application and use enum status for approved/rejected

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -1,4 +1,8 @@
 class PetApplication < ApplicationRecord
   belongs_to :pet
   belongs_to :application
+
+  validates_presence_of :pet_id, :application_id
+
+  enum status: [:approved, :rejected]
 end
