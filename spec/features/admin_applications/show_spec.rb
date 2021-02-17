9c34d9cb310a15ebb945bd86e5f542603d8f0c24
Katy La Tour <klatour324@gmail.com>
2021-02-17 13:17:10 -0600

    Update feature spec for admin_controllers show spec with setup and commenting out rejected spec

--- spec/features/admin_applications/show_spec.rb
+++ spec/features/admin_applications/show_spec.rb
@@ -26,6 +26,13 @@ RSpec.describe "Admin Application Show Page" do
                                  sex: 0)
     @application = create(:application)
     @application.pets << [@pet_1, @pet_2]
+
+    # @shelter_1 = create(:shelter, id: 1)
+    # @pet_1 = create(:pet, id: 1, shelter_id: 1, name: "Nikita")
+    # @pet_2 = create(:pet, id: 2, shelter_id: 1, name: "Kiko")
+    # @application = create(:application, id: 1, application_status: "Pending")
+    # @pet_application_1 = create(:pet_application, application_id: 1, pet_id: 1 )
+    # @pet_application_2 = create(:pet_application, application_id: 1, pet_id: 2 )
   end
 
   it "can approve a pet for adoption" do
@@ -52,4 +59,32 @@ RSpec.describe "Admin Application Show Page" do
       expect(page).to have_content("Pet Approved")
     end
   end
+
+  # it "can reject a pet for adoption" do
+  #   visit "/admin/applications/#{@application.id}"
+  #
+  #   within "#pet-#{@pet_1.id}" do
+  #     expect(page).to have_content(@pet_1.name)
+  #     expect(page).to have_button("Reject")
+  #
+  #     click_button("Reject")
+  #
+  #     expect(current_path).to eq("/admin/applications/#{@application.id}")
+  #     expect(page).to_not have_button("Approve")
+  #     expect(page).to_not have_button("Rejcet")
+  #     expect(page).to have_content("Pet Rejected")
+  #   end
+  #
+  #   within "#pet-#{@pet_2.id}" do
+  #     expect(page).to have_content(@pet_2.name)
+  #     expect(page).to have_button("Reject")
+  #     click_button("Reject")
+  #
+  #
+  #     expect(current_path).to eq("/admin/applications/#{@application.id}")
+  #     expect(page).to_not have_button("Approve")
+  #     expect(page).to_not have_button("Reject")
+  #     expect(page).to have_content("Pet Rejected")
+  #   end
+  # end
 end
