8971e1da5246338b19a52d5000988fd82534c0f1
Katy La Tour <klatour324@gmail.com>
2021-02-17 21:04:26 -0600

    Fix update action to change status on application if all pets have been approved/rejected

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -8,6 +8,12 @@ class AdminApplicationsController < ApplicationController
     pet_application = PetApplication.find(params[:pet_application_id])
     pet_application.update(pet_application_params)
 
+    if @application.pet_applications.all? == "Approved"
+      @application.update(application_status: "Approved")
+    else
+      @application.update(application_status: "Rejected")
+    end
+    @application.save
     redirect_to "/admin/applications/#{params[:id]}"
   end
 
